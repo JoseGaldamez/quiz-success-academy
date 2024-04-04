bb1b19be96e1d488969329fb8b4a5b437c2a1645
Jose Galdamez <josegaldamez1991@gmail.com>
2024-04-04 10:29:41 -0600

    first commit

---
+++ src/common/MenuBar.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import styles from "./Common.module.css";
+import Link from 'next/link';
+
+export const MenuBar = () => {
+    return (
+        <div className={styles['menu-blue']} >
+            <section className={styles.menu}>
+                <span>
+                    {/* eslint-disable-next-line @next/next/no-img-element */}
+                    <img className={styles.LogoPrincipal} src="https://successacademyhn.com/wp-content/uploads/2023/02/logo_succress-blanco-copia.png" alt="logo" />
+                </span>
+                <Link href={'/'} className={styles.contactButton}>
+                    Solicitar acceso
+                </Link>
+            </section>
+        </div>
+    )
+}
---
+++ src/common/TopBar.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+import { ImLocation2, ImMail4, ImPhone } from "react-icons/im";
+
+
+import styles from "./Common.module.css";
+
+export const TopBar = () => {
+    return (
+        <div className={styles['top-orange']}>
+            <ul className={styles['list']}>
+                <li className={styles['menu-item']}>
+                    <ImLocation2 />
+                    Plaza universitaria, frente a Unah-VS, SPS</li>
+                <li className={styles['menu-item']}>
+                    <ImLocation2 />
+                    Edificio Honduplaza, 2do Nivel, El Progreso, Yoro</li>
+                <li className={styles['menu-item']}>
+                    <ImMail4 />
+                    info.cursos@successacademyhn.com</li>
+                <li className={styles['menu-item']}>
+                    <ImPhone />
+                    +504 9518-2139</li>
+                <li className={styles['menu-item']}>
+                    <ImPhone />
+                    +504 9767-2191</li>
+            </ul>
+        </div>
+    )
+}
