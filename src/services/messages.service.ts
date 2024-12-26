b8e0117f0b1548c73cd081e90d910ab2a7549406
José Galdámez <josegaldamez1991@gmail.com>
2024-12-26 17:35:13 -0600

    sendmails

--- src/services/messages.service.ts
+++ src/services/messages.service.ts
@@ -1,27 +1,37 @@
-const nodemailer = require("nodemailer");
+// const nodemailer = require("nodemailer");
 
-const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: 465,
-    secure: true,
-    auth: {
-        user: process.env.EMAIL_USERNAME,
-        pass: process.env.EMAIL_PASSWORD,
-    },
-});
+// const transporter = nodemailer.createTransport({
+//     host: process.env.EMAIL_HOST,
+//     port: 465,
+//     secure: true,
+//     auth: {
+//         user: process.env.EMAIL_USERNAME,
+//         pass: process.env.EMAIL_PASSWORD,
+//     },
+// });
 
 export const sendMessage = async (mesageBody: string) => {
     const messageOptions = {
         from: `"Success Academy Quiz" <${process.env.EMAIL_USERNAME}>`,
         to: "coordinacion.academica@successacademyhn.com,josegaldamez1991@gmail.com,Successacademy0101@gmail.com",
         subject: "Student has finished the quiz",
-        text: mesageBody,
+        content: mesageBody,
     };
 
-    return await transporter.sendMail(
-        messageOptions,
-        (error: any, info: any) => {
-            return { error, info };
-        }
-    );
+    await fetch("https://send-mails-api-next-js.vercel.app/api/sendEmail", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(messageOptions),
+    });
+
+    // return await transporter.sendMail(
+    //     messageOptions,
+    //     (error: any, info: any) => {
+    //         return { error, info };
+    //     }
+    // );
+
+    return { success: true, message: "Message sent" };
 };
